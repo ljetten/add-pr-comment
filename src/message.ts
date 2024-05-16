be7ef5f84730da527be861418292668cd7e5d348
ljetten <l.jetten@portofrotterdam.com>
2024-05-16 12:24:52 +0200

    more linting

--- src/message.ts
+++ src/message.ts
@@ -71,7 +71,7 @@ export async function getMessageFromPath(searchPath: string) {
 
   // return trimmed message if message is too long (maximum is 65536 characters)
   return message.length > maxCharacterLength
-    ? message.substring(0, maxCharacterLength - 3) + "..."
+    ? message.substring(0, maxCharacterLength - 3) + '...'
     : message
 }
 
